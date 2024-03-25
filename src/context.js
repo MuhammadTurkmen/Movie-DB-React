4c2f18ff63e92fe216fa354c25b2772df4bf8a4e
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-25 20:08:25 +0430

    init

--- src/context.js
+++ src/context.js
@@ -4,34 +4,11 @@ export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState({show:false, msg:''})
-  const [movies, setMovies] = useState([])
+
   const [query, setQuery] = useState('superman')
   
 
-  const fetchMovies = async (url) => {
-    setIsLoading(true) 
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-
-      if(data.Response === 'True') {
-        setMovies(data.Search)
-        setError({show: false, msg: ''})
-      } 
-      else {
-        setError({show: true, msg: data.Error})
-      }
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error);
-    }
-  }
   
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`)
-  }, [query])
 
   
   return <AppContext.Provider value={{
