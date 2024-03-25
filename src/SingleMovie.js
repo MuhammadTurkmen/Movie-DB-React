a093951a5202b03b086571431f9bcf46f999c9e7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-25 10:38:16 +0430

    init

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -6,26 +6,7 @@ import useFetch from './useFetch'
 
 const SingleMovie = () => {
   const { id } = useParams()
-  const [movie, setMovie] = useState({})
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setErorr] = useState({show: false, msg:''})
-
-  const fetchMovie = async (url) => {
-    const response =  await fetch(url)
-    const data = await response.json()
-    if(data.Reasponse === 'False') {
-      setErorr({show: true, msg: data.Error})
-      setIsLoading(false)
-    }   
-    else { 
-      setMovie(data)
-      setIsLoading(false) 
-    }
-  }
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`)
-  }, [id])
+  
 
   if(isLoading) {
     return <div className='loading'></div>
