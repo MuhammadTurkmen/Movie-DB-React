b9e2c3c07530ab1db74568c97467d9533c85a3cd
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-25 20:09:37 +0430

    init

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,7 +3,7 @@ import React, { useState, useContext, useEffect } from 'react'
 const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_Key}`
 
 
- const useFetch = () => {
+ const useFetch = (urlPrams) => {
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState({show:false, msg:''})
   const [movies, setMovies] = useState([])
@@ -30,7 +30,7 @@ const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MO
   useEffect(() => {
     fetchMovies(`${API_ENDPOINT}&s=${query}`)
   }, [query])
-  
+
   return
 }
 
