f6f5b769a515154a96f7457145d0bdc5f7fa5102
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-25 19:58:49 +0430

    init

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,5 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react'
 
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_Key}`
+const AppContext = React.createContext()
+
+
 export const useFetch = () => {
   return (
     <div>useFetch</div>
