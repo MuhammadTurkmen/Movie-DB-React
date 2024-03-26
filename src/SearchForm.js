b7e4db31fe94a04c31f7c8b26263f06516b0f61a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-26 16:46:36 +0430

    init

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -4,6 +4,7 @@ const SearchForm = () => {
   const {query, setQuery, error} = useGlobalContext()
   return <form className='search-form' onSubmit={(e) => e.preventDefault()}>
     <h2>search movies</h2>
+    <input type="text" className='form-input' />
   </form>
 }
  
