b9a1b3b96c89b0c297b92709c81a7388c2ad0296
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-26 16:38:25 +0430

    init

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { useGlobalContext } from './context'
 const SearchForm = () => {
   const {query, setQuery, error} = useGlobalContext()
-  return <form className='search-form' onSubmit={(e) => e.preventDefault()}>search component</form>
+  return <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+    <h2>search movies</h2>
+  </form>
 }
  
 export default SearchForm
