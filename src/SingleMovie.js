88e6ed87962ed7bf8a0cc100fe4d5fb477ec16d7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-03-26 10:39:12 +0430

    init

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -6,7 +6,7 @@ import useFetch from './useFetch'
 
 const SingleMovie = () => {
   const { id } = useParams()
-  
+  const {} = useFetch()
 
   if(isLoading) {
     return <div className='loading'></div>
