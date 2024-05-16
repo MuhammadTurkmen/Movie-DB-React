0f04882d5de1c033ae7383753fedf9bddc0fb5a7
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-05-16 15:02:59 +0430

    adding state

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -6,6 +6,7 @@ const SingleMovie = () => {
   const { id } = useParams()
   const [movie, setMovie] = useState({})
   const [isLoading, setIsLoading] = useState(true)
+  const [error, setErorr] = useState({show: false, msg:''})
   return <h2>single movie</h2>
 }
 
